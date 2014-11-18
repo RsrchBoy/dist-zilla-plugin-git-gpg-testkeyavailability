fcb2241b328ec068285c82038d9d77c718834be0
Chris Weyl <cweyl@alumni.drew.edu>
2014-11-17 17:12:24 -0800

    initial commit

---
+++ lib/Dist/Zilla/Plugin/Git/GPG/TestKeyAvailability.pm
@@ -0,0 +1,19 @@
+package Dist::Zilla::Plugin::Git::GPG::TestKeyAvailability;
+
+# ABSTRACT: The great new Dist::Zilla::Plugin::Git::GPG::TestKeyAvailability!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
